93883dd3e25b63e74197320402a56be1432f0252
Nick <nthornton419@gmail.com>
2021-02-04 14:53:15 +0000

    tidy-up

--- rtcSet.c
+++ rtcSet.c
@@ -2,12 +2,6 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
 
 int main()
 {
@@ -16,8 +10,6 @@ int main()
     const uint LED_PIN = 25;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-
-    printf("Hello, world!\n\r");
 
     while (true)
     {
